68d8feabefc7bf92d2a8f844f4f398aa547567a5
danielferreiradf <danielferreiradk@gmail.com>
2020-03-08 10:10:10 +0000

    installed neat-csv package to read csv files, loaded them in searchController

--- app/controllers/SearchController.js
+++ app/controllers/SearchController.js
@@ -1,7 +1,23 @@
+const neatCsv = require("neat-csv");
+const path = require("path");
+const { promises: fs } = require("fs");
+
 class SearchController {
   async searchItems(req, res) {
     try {
-      return res.send("/search");
+      // Destructures searchTerm from req.query
+      const { searchTerm } = req.query;
+
+      const loadedItemsFromCSV = await fs.readFile(
+        path.join(__dirname, "..", "..", "database/items.csv")
+      );
+      const loadedUsersFromCSV = await fs.readFile(
+        path.join(__dirname, "..", "..", "database/users.csv")
+      );
+      const parsedItems = await neatCsv(loadedItemsFromCSV);
+      const parsedUsers = await neatCsv(loadedUsersFromCSV);
+
+      return res.json(searchTerm);
     } catch (error) {
       return res.status(500).json({ error: error.message });
     }
